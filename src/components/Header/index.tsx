bd80e161f53a2e768a869743aebcdb42d0ac0860
vitorvf <vitorferreiravf2@gmail.com>
2023-05-28 02:00:51 -0300

    atualizacoes menores

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,11 @@ import logoSrc from "../../assets/branco.webp";
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logoSrc} alt="logo" />
+      <img
+        src={logoSrc}
+        alt="logo"
+        style={{ width: "174px", height: "87px" }}
+      />
     </HeaderContainer>
   );
 }
