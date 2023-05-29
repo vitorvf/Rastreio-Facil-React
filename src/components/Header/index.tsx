d414593f4778ca184c2bbf574ea9bbe0beb01900
vitorvf <vitorferreiravf2@gmail.com>
2023-05-29 16:39:46 -0300

    update-final

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import { HeaderContainer } from "./styles";
 import logoSrc from "../../assets/branco.webp";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img
-        src={logoSrc}
-        alt="logo"
-        style={{ width: "174px", height: "87px" }}
-      />
+      <Link to={"/"}>
+        <img
+          src={logoSrc}
+          alt="logo"
+          style={{ width: "174px", height: "87px" }}
+        />
+      </Link>
     </HeaderContainer>
   );
 }
